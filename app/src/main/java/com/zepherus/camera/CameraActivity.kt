2a86fb136144249635afc03f79ebc3eee3b25675
Andrei Fomin <vfvfns2010@mail.ru>
2023-12-09 11:12:21 +0300

    Add project

---
+++ app/src/main/java/com/zepherus/camera/CameraActivity.kt
@@ -0,0 +1,133 @@
+package com.zepherus.camera
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.zepherus.camera.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executor
+
+private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss"
+
+class CameraActivity : AppCompatActivity() {
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var executor: Executor
+    private lateinit var binding: ActivityMainBinding
+
+    private val name =
+        SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+            if (map.values.all { it }) {
+                startCamera()
+            } else {
+                Toast.makeText(this, R.string.info_permission_not_granted, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        executor = ContextCompat.getMainExecutor(this)
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+
+        checkPermissions()
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        ).build()
+
+        imageCapture.takePicture(
+            outputOptions, executor, object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "${getString(R.string.info_photo_saved)} ${outputFileResults.savedUri}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    Glide.with(this@CameraActivity).load(outputFileResults.savedUri).circleCrop()
+                        .into(binding.imgPhoto)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "${getString(R.string.info_photo_failed)} ${exception.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    exception.printStackTrace()
+                }
+            }
+        )
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build()
+            preview.setSurfaceProvider(binding.previewPhoto.surfaceProvider)
+            imageCapture = ImageCapture.Builder().build()
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                this,
+                CameraSelector.DEFAULT_BACK_CAMERA,
+                preview,
+                imageCapture
+            )
+        }, executor)
+    }
+
+    private fun checkPermissions() {
+        val isAllGranted = REQUEST_PERMISSIONS.all { permission ->
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        }
+        if (isAllGranted) {
+            startCamera()
+        } else {
+            launcher.launch(REQUEST_PERMISSIONS)
+        }
+    }
+
+    companion object {
+        private val REQUEST_PERMISSIONS: Array<String> = buildList {
+            add(Manifest.permission.CAMERA)
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+    }
+}
